b4d352947515b6716e999f6374d6e77d55531ee1
Aaadeleee <167509022+Aaadeleee@users.noreply.github.com>
2024-04-23 13:02:58 +0200

    Update transformation.h

--- transformation.h
+++ transformation.h
@@ -37,8 +37,42 @@ typedef struct Liste_Clause_ {
 } Liste_Clause;
 
 
-void lire_sudoku ();
+// ?? TYPES FOR THE 2ND TYPE OF FORMULA ?? (variables are just integers (+ or -))
 
-void creer_dimacs (Liste_Clause LC, char* file_name);
+typedef struct Cellule_Liste2_ {
+    int var;   
+	struct Cellule_Liste2_* suiv;
+} Cellule2;
+
+
+typedef struct Liste2_ {
+	unsigned int size;       
+	Cellule_Liste2_ *first; 
+	//Cellule_Liste2_ *last;  
+	                       
+} Liste2;
+
+
+// TYPES FOR THE CLAUSE ITSELF
+
+typedef struct Cellule_Liste_Clause2_ {
+    Liste2 clause;   
+	struct Cellule_Liste_Clause2_* suiv;
+} Cellule_Clause2;
+
+
+typedef struct Liste_Clause2_ {
+	unsigned int size;  //nb of clauses in the formula     
+	unsigned int nb_var;
+	Cellule_Liste_Clause2_ *first; 
+	Cellule_Liste_Clause2_ *last;  
+	                       
+} Liste_Clause2;
+
+
+
+void read_sudoku ();
+
+void create_dimacs (Liste_Clause2 LC, char* file_name);
 
 #endif
