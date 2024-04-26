3bd5741de2d6a7a845dea3f6d032145f24d479ed
andersontess <132293882+andersontess@users.noreply.github.com>
2024-04-26 23:54:15 +0200

    Update transformation.h

--- transformation.h
+++ transformation.h
@@ -12,6 +12,9 @@ Liste_D* read_sudoku (FILE *f, Liste_D *Grid);
 // Function just to verify the list is correctly formed
 void afficher_liste(Liste_D *L);
 
+bool is_number_in_lcr (Cellule_D *cell, Liste_D *Grid);
+
+Liste_C* construct_clause(Liste_D* sudoku);
 // Create the clauses of each case in the grid
 //Liste_C* construct_clause(Liste_D *Se, Liste_D *Grid);
 
