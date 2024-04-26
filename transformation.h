008a37e63b5c6fba7f4f66dcb757ba4124e1f8dc
SofiG30 <102640121+SofiG30@users.noreply.github.com>
2024-04-26 22:52:00 +0200

    Update transformation.h

--- transformation.h
+++ transformation.h
@@ -1,87 +1,24 @@
 #ifndef _TRANSFORMATION_H_
 #define _TRANSFORMATION_H_
 #include <stdio.h>
-
-
-// TYPES FOR WHAT IS INSIDE THE CLAUSES
-
-typedef struct Cellule_Liste_ {
-	//cell = (li, col, el)
-	int li;
-    int col;
-    int el; 
-	int reg ;
-	struct Cellule_Liste_* suiv;
-} Cellule;
-
-
-typedef struct Liste_ {
-	unsigned int size_grid;
-	unsigned int length;       
-	Cellule *first; 
-	Cellule *last;                         
-} Liste;
+#include "types.h"
+          
 
 
 
+// Reads an instance of a sudoku file
+Liste_D* read_sudoku (FILE *f, Liste_D *Grid);
 
-// TYPES FOR THE CLAUSE ITSELF
+// Function just to verify the list is correctly formed
+void afficher_liste(Liste_D *L);
 
-typedef struct Cellule_Liste_Clause_ {
-    Liste data;   
-	struct Cellule_Liste_Clause_* suiv;
-} Cellule_Clause;
+// Create the clauses of each case in the grid
+//Liste_C* construct_clause(Liste_D *Se, Liste_D *Grid);
 
+Liste_Di_2* rewrite_var(Liste_C *L);
 
-typedef struct Liste_Clause_ {
-	unsigned int size;       
-	Cellule_Clause *first; 
-	Cellule_Clause *last;  
-	                       
-} Liste_Clause;
+void create_dimacs (Liste_Di_2* LC, char* file_name);
 
 
-// ?? TYPES FOR THE 2ND TYPE OF FORMULA ?? (variables are just integers (+ or -))
-
-typedef struct Cellule_Liste2_ {
-    int var;   
-	struct Cellule_Liste2_* suiv;
-} Cellule2;
-
-
-typedef struct Liste2_ {
-	unsigned int size;       
-	Cellule2 *first; 
-	//Cellule_Liste2_ *last;  
-	                       
-} Liste2;
-
-// TYPES FOR THE CLAUSE ITSELF
-typedef struct Cellule_Liste_Clause2_ {
-    Liste2 clause;   
-	struct Cellule_Liste_Clause2_* suiv;
-} Cellule_Clause2;
-
-
-typedef struct Liste_Clause2_ {
-	unsigned int size;  //nb of clauses in the formula     
-	unsigned int nb_var;
-	Cellule_Clause2 *first; 
-	Cellule_Clause2 *last;  
-	                       
-} Liste_Clause2;
-
-
-Liste* init_liste();
-
-void add_cell(Liste *L, Cellule *c) ;
-
-Liste* read_sudoku (FILE *f, Liste *Grid);
-
-void afficher_liste(Liste *L);
-
-Liste_Clause2* construct_clause (Liste *L);
-
-void create_dimacs (Liste_Clause2 LC, char* file_name);
 
 #endif
