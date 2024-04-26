784fd441402e26f0a059a78835c81496d2447656
SofiG30 <102640121+SofiG30@users.noreply.github.com>
2024-04-26 22:56:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "dict.h"
+#include "types.h"
 #include "transformation.h"
 
 int main(int argc, char *argv[]) {
@@ -16,6 +19,31 @@ int main(int argc, char *argv[]) {
     
     Liste *Se = read_sudoku(f);
     afficher_liste(Se);
+
+    /* Test for rewrite_var function
+ 
+    Liste_C *L = init_listeClause();
+    for (int j = 0 ; j < 3 ; j++) {
+        // printf("Error here\n");
+        
+        Liste_D *clause = init_liste(); 
+
+        for (int i = 0; i < 3; i++) {
+            Cellule_D *new_cell = malloc(sizeof(Cellule_D));
+            printf("Add values: \n");
+            scanf("%d" , &new_cell->li);
+            scanf("%d" , &new_cell->col);
+            scanf("%d" , &new_cell->el);
+            scanf("%d" , &new_cell->reg);
+            new_cell->suiv = NULL;
+            add_cell(clause, new_cell);
+        }
+        add_cell_Clause(L, clause);
+    }
+Liste_Di_2 *final_list = rewrite_var(L);
+
+create_dimacs(final_list, argv[1]); */
+
     return 0;
 
 }
