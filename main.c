9a480418a467c6097c1e29850e4cc2e3f4ff457a
SofiG30 <102640121+SofiG30@users.noreply.github.com>
2024-05-03 19:10:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -23,43 +23,44 @@ int main(int argc, char *argv[]) {
     Liste_D *Se = init_liste();
     Liste_C *pos_cl = init_empty_clause();
     Liste_C *neg_cl = init_empty_clause();
-   // dictionary *D = init_dict();
-   // Liste_Di_2 *pos = init_listeDi_2();
-   // Liste_Di_2 *neg = init_listeDi_2();
+    dictionary *D = init_dict();
+    Liste_Di_2 *pos = init_listeDi_2();
+    Liste_Di_2 *neg = init_listeDi_2();
 
     // We start by reading the sudoku grid
     Se = read_sudoku(f, Grid);
-    // Create the clause lists 
+   // printf("Se\n");
+    //afficher_liste_D(Se);
+    //printf("\n");    // Create the clause lists 
     // Positive 
     pos_cl = positive_clauses(Grid, Se);    
-    afficher_liste_C(pos_cl);
-    //printf("New list -> neg list\n");
-    // Negative
-    //printf("Error\n");
+    //afficher_liste_C(pos_cl);
+    //printf("\n");
+    
     neg_cl = construct_clause_neg(pos_cl, Se);  
-    // No entiendo por que no funcionaaa
-       //afficher_liste_C(neg_cl);
+    afficher_liste_C(neg_cl);
 
-    /*
+
     // Turn the lists into Di_lists (Only integers represent the cases) + store in dict
-     printf("Error\n");
     pos = rewrite_var(pos_cl,D);
-     printf("Error\n");
+    //afficher_liste_Di2(pos); 
+    //printf("Negative clauses\n");
     neg = rewrite_var(neg_cl, D);
+    //afficher_liste_Di2(neg);
 
+    // Continue from here 
     // Write the dimacs file 
-    create_dimacs(pos, neg, argv[2]);
-    */
+   // create_dimacs(pos, neg, argv[2]);
 
     // Free all the lists -> prevent memory problems
     free(Se);
     free(Grid);
     free(pos_cl);
     free(neg_cl);
-   // free(pos);
-   // free(neg);
+    free(pos);
+    free(neg);
+    detruire_dict(D);
     fclose(f);
 
 return 0;
 }
-
